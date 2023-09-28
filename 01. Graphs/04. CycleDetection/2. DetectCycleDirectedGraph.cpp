163f454443d78cc7a7831b04de888ab07361c358
MeetSinojia <71940146+MeetSinojia@users.noreply.github.com>
2023-09-28 16:25:56 +0530

    Update 2. DetectCycleDirectedGraph.cpp

--- 01. Graphs/04. CycleDetection/2. DetectCycleDirectedGraph.cpp
+++ 01. Graphs/04. CycleDetection/2. DetectCycleDirectedGraph.cpp
@@ -1,37 +1,44 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-bool detectCycle(unordered_map<int, vector<int>>& m, unordered_set<int>& visited, int node, int parent, unordered_set<int>& bag) {
-	visited.insert(node);
-	bag.insert(node);
-	for(auto nbr: m[node]) {
-	    if(bag.find(nbr) != bag.end()) return true;
-		else if(visited.find(nbr) == visited.end()) {
-	        if(detectCycle(m, visited, nbr, node, bag)) return true;
-		}
-	}
-	bag.erase(node);
-	return false;
-}
-
-bool solve(int n, vector<vector<int> > edges) 
-{	
-	unordered_map<int, vector<int>> graph;
-
-	for(auto a: edges) {
-		graph[a[0]].push_back(a[1]);
-	}
-	unordered_set<int> visited;
-	unordered_set<int> bag;
-	for(int i = 1; i < n; i++) {
-	    if(visited.find(i) == visited.end()) {
-	        if(detectCycle(graph, visited, i, -1, bag)) {
-	            return true;
-	        }
-	    }
-	    
-	}
-	return false;
-	
-
-}
+class Solution {
+    
+  public:
+    bool dfs(int node,vector<int> adj[],int vis[],int pvis[])
+    {
+        vis[node]=1;
+        pvis[node]=1;
+        
+        for(auto it:adj[node])
+        {
+            if(!vis[it])
+            {
+                if(dfs(it,adj,vis,pvis)==true)
+                {
+                    return true;
+                }
+            }
+            
+            else if(vis[it] && pvis[it])
+            {
+                return true;
+            }
+        }
+        pvis[node]=0;
+        return false;
+    }
+    
+  public:
+    // Function to detect cycle in a directed graph.
+    bool isCyclic(int V, vector<int> adj[]) {
+        int vis[V]={0};
+        int pvis[V]={0};
+        
+        for(int i=0;i<V;i++)
+        {
+            if(dfs(i,adj,vis,pvis)==true)
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
